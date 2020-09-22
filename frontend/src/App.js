4d1687a66d79061667f669f1954ead658322842e
Ramazan Emre Osmanoglu <ramazanemreosmanoglu@gmail.com>
2020-09-22 13:55:37 +0300

    frontend second commit

---
+++ frontend/src/App.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import './App.css'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams,
+  useRouteMatch
+} from "react-router-dom";
+
+import {
+  Navbar,
+  Button,
+  Nav,
+  NavDropdown,
+  Form,
+  FormControl,
+  Container,
+} from 'react-bootstrap';
+import Home from './Home.jsx';
+
+function App() {
+  return (
+    <Router>
+        <Navbar bg="primary" expand="lg" variant='dark'>
+          <Navbar.Brand href="#home">Sozluk</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="#">Ana Sayfa</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Form inline>
+              <FormControl type="text" placeholder="Arama" className="mr-sm-2" />
+              <Button variant="outline-light">Arama</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Navbar>
+
+        <Switch>
+          <Route exact path="/" component={Home}/>
+        </Switch>
+    </Router>
+  );
+}
+
+export default App;
---
+++ frontend/src/Home.jsx
@@ -0,0 +1,66 @@
+import {
+    Navbar,
+    Button,
+    Nav,
+    NavDropdown,
+    Form,
+    FormControl,
+    Table,
+    Container,
+} from 'react-bootstrap';
+import React from 'react';
+import './Home.css';
+
+var example_data = [
+    {
+      'id': 1,
+      'title': 'Menemen',
+      'date': '40.13.2021',
+    },
+    {
+        'id': 2,
+        'title': 'Kebap',
+        'date': '34.13.2021',
+    },
+    {
+        'id': 3,
+        'title': 'Deneme',
+        'date': '33.13.2021',
+    },
+    {
+        'id': 4,
+        'title': 'Python',
+        'date': '31.13.2021',
+    },
+];
+
+function generate_row(data) {
+    return (
+        <tr>
+            <td>{data.id}</td>
+            <td>{data.title}</td>
+            <td>{data.date}</td>
+        </tr>
+    )
+}
+
+export default function Home() {
+
+
+    return (
+        <Container>
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Baslik</th>
+                        <th>Tarih</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {example_data.map(generate_row)}
+                </tbody>
+            </Table>
+        </Container>
+    );
+}
