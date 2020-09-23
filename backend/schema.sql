e5c700bb882a44785dda58fa606c1522d9ec9334
EkremDincel <ekremdincel@gmail.com>
2020-09-23 13:04:17 +0300

    Update schema.sql.

--- backend/schema.sql
+++ backend/schema.sql
@@ -1,1 +1,3 @@
 CREATE TABLE IF NOT EXISTS users (hash BLOB, name TEXT, email TEXT);
+CREATE TABLE IF NOT EXISTS topics (name TEXT, entries BLOB);
+CREATE TABLE IF NOT EXISTS entries (user_id INTEGER, topic_id INTEGER, date TEXT, content TEXT);
