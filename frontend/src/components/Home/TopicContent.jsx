992ec5051e371ff3c88df975b32420ab3d6158a5
Emre <ramazanemreosmanoglu>
2020-09-25 13:28:50 +0300

    Add Entries

--- frontend/src/components/Home/TopicContent.jsx
+++ frontend/src/components/Home/TopicContent.jsx
@@ -1,4 +1,26 @@
 import React from 'react';
+import Entry from './Entry.jsx';
+
+var example_data = [
+  {
+    id: 1,
+    content: 'Lorem Ipsum Dolor Sit Amet',
+    user: 'ramazanemre',
+    date: '12.03.2021 Pazar'
+  },
+  {
+    id: 2,
+    content: 'Basit bir deneme',
+    user: 'admin',
+    date: '10.03.2021 Cuma',
+  },
+];
+
+function generate_entry(data) {
+  return (
+    <Entry content={data.content} username={data.user} date={data.date}/>
+  )
+}
 
 export default class TopicContent extends React.Component {
   state = {
@@ -7,7 +29,9 @@ export default class TopicContent extends React.Component {
   render() {
     return (
       <React.Fragment>
-        {this.decoded}
+        <h1>{this.decoded}</h1>
+
+        {example_data.map(generate_entry)}
       </React.Fragment>
     )
   }
@@ -16,7 +40,7 @@ export default class TopicContent extends React.Component {
     super(props);
     var name = window.location.pathname.split('/')[2];
     if(name==undefined){
-      var name = 'Merhaba!';
+      this.decoded = 'Merhaba!';
     }
     else{
       this.decoded = decodeURIComponent(name);
