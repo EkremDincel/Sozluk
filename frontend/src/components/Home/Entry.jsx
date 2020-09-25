d6487a150648eebcda0b65096dc9e4701c83d6e0
Emre <ramazanemreosmanoglu>
2020-09-25 13:28:07 +0300

    Change CSS Styles

--- frontend/src/components/Home/Entry.jsx
+++ frontend/src/components/Home/Entry.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import {
     Card,
+    Row,
+    Col,
 } from 'react-bootstrap';
+import './Home.css';
 
 export default class Entry extends React.Component {
     render() {
@@ -10,11 +13,18 @@ export default class Entry extends React.Component {
           <Card>
             <Card.Body>
               <Card.Text>{this.props.content}</Card.Text>
-              <div className='entry-username'>{this.props.username}</div>
             </Card.Body>
   
             <Card.Footer>
-              <small className='text-muted'>{this.props.date}</small>
+              <Row>
+                <Col>
+                  <small className='text-muted'>{this.props.date}</small>
+                </Col>
+
+                <Col className='entry-username'>
+                  <small className='text-muted entry-username'>{this.props.username}</small>
+                </Col>
+              </Row>
             </Card.Footer>
           </Card>
         </React.Fragment>
